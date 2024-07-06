ed12abd26199f6c844966f06674be858db11a2bc
kelechi055 <157651060+kelechi055@users.noreply.github.com>
2024-07-06 19:25:39 -0400

    added animations

--- app/testingPage1/page.tsx
+++ app/testingPage1/page.tsx
@@ -1,26 +1,39 @@
 import Image from "next/image";
+import Head from 'next/head';
 import Link from "next/link";
-import gamestock1 from "../public/gamestock1.png";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
-      <h1 className="text-2xl">
+    <main> 
+      <Head>
+        <link rel="preload" 
+        href="C:\Users\kelec\Downloads\WeCracked\WeCrackedHackathon\app\globals.css" />
+      </Head>
+      <div className="text=4xl">
+      <div></div>
+      <Image src= "/gamestock logo.png" alt = "Logo" width={70} height={70} layout="fixed" />
+      </div>
+      <h1 className="flex flex-col items-center justify-center text=2xl">
         <br></br>
         <br></br>
         <br></br>
         <br></br>
         <br></br>
         <br></br>
-      <Image src= "/gamestock2.png" alt = "GameStock Logo" 
-        width={700} height={500}/>
-        <br></br>
+      <div >
+        <Image src= "/gamestock2.png" alt = "GameStock Logo" 
+          width={700} height={500} layout="fixed"/>
+          <br></br>
+      </div>
       </h1>
       
         <h2>
           <div className="flex justify-center " style={{ fontSize: '1.5rem'}}>
+            <div className = "slide-down">
             Power up your portfolio with video game stock investments. Roblox, Nintendo, etc.
           </div>
+          </div>
+          
         <div className="fade-in">
         <br></br>
         <br></br>
