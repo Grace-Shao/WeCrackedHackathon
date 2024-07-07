3cdbd96eecc2cef43d7924581c303c76da1de0a6
Grace-Shao <gshao8@gatech.edu>
2024-07-06 22:55:39 -0400

    news site

--- app/detailedStockPage/[id]/page.tsx
+++ app/detailedStockPage/[id]/page.tsx
@@ -12,17 +12,42 @@ const getQuote = async (symbol) => {
   }
 };
 
+const getNews = async (symbol) => {
+  try {
+    const res = await fetch(`https://finnhub.io/api/v1/company-news?symbol=${symbol}&from=2024-06-01&to=2024-07-01&token=${API_KEY}`);
+    return res.json();
+  } catch (error) {
+    console.log("Error loading news: ", error);
+  }
+}
+
 export default async function ViewStock({ params }) {
   const { id } = params;
   const quote = await getQuote(id);
+  const news = await getNews(id);
   return (
-    <div className="flex justify-center m-16">
-      <div>
-        <h1 className="text-4xl">{id} Quote Price</h1>
-        <p className="text-2xl">Current Price: {quote.c}</p>
-        <p className="text-2xl">High: {quote.h}</p>
-        <p className="text-2xl">Low: {quote.l}</p>
-      </div>
+    <div className="max-w-full mx-auto py-8 px-4 bg-gray-900">
+    <div className="mb-10">
+      <h1 className="text-4xl font-bold text-white mb-4">{id} Quote Price</h1>
+      <p className="text-2xl text-gray-300">Current Price: <span className="font-semibold text-white">{quote.c}</span></p>
+      <p className="text-2xl text-gray-300">High: <span className="font-semibold text-white">{quote.h}</span></p>
+      <p className="text-2xl text-gray-300">Low: <span className="font-semibold text-white">{quote.l}</span></p>
     </div>
+    <div>
+      <h1 className="text-4xl font-bold text-white mb-6">News</h1>
+      <ul>
+        {news.map((article, key) => (
+          <li key={key} className="max-w-4xl mb-4 bg-gray-800 shadow-lg rounded-lg overflow-hidden">
+            <div className="p-6">
+              <h2 className="text-2xl font-bold text-white mb-2">
+                <a href={article.url} className="hover:text-blue-400 transition duration-300 ease-in-out text-white">{article.headline}</a>
+              </h2>
+              <p className="text-gray-400">{article.summary}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
   );
 }
