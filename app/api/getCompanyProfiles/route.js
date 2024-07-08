5b0690f471f0c82fdbeb169b63a3667ff02ea0f7
Grace-Shao <gshao8@gatech.edu>
2024-07-08 16:24:34 -0400

    fixed deploy issues

--- app/api/getCompanyProfiles/route.js
+++ app/api/getCompanyProfiles/route.js
@@ -1,37 +1,21 @@
 import { NextResponse } from "next/server";
-const API_KEY = 'cq4p1i9r01qhh2h69aigcq4p1i9r01qhh2h69aj0';
-// save to json file
-// have the list of companies displayed
-// when entering companies, then fetch url
-// const symbols = [
-//   'NTDOY', 'NTES', 'EA', 'TTWO', 'RBLX', 'NEXOY', 'CCOEY', 'BILI', 
-//   'PLTK', 'UBSFY', 'DDI', 'GRVY', 'SOHU', 'MYPS', 'GMGI', 'SKLZ', 
-//   'BHAT', 'GAME', 'TRUG', 'GDC', 'MSGM', 'GXAI'
-// ];
-const symbols = [
-  'NTDOY', 'NTES', 'EA',
-];
-
-const fetchCompanyProfile = async (symbol) => {
-    const url = `https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${API_KEY}`;
-    try {
-      const response = await fetch(url);
-      const data = await response.json(); // Ensure this waits for the JSON to be read
-      console.log(`${symbol}:`, data);
-      return data;
-    } catch (error) {
-      console.error(`Error fetching company profile for ${symbol}:`, error);
-      return null;
-    }
-  };
-
-// Import necessary modules
-import { NextApiRequest, NextApiResponse } from 'next';
 import yahooFinance from 'yahoo-finance2';
-import { NextResponse } from 'next/server';
 
-// Define the symbols you want to fetch data for
-const symbols = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA'];
+const API_KEY = 'cq4p1i9r01qhh2h69aigcq4p1i9r01qhh2h69aj0';
+
+
+// const fetchCompanyProfile = async (symbol) => {
+//     const url = `https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${API_KEY}`;
+//     try {
+//       const response = await fetch(url);
+//       const data = await response.json(); // Ensure this waits for the JSON to be read
+//       console.log(`${symbol}:`, data);
+//       return data;
+//     } catch (error) {
+//       console.error(`Error fetching company profile for ${symbol}:`, error);
+//       return null;
+//     }
+//   };
 
 // Placeholder function for fetching company profile
 async function fetchCompanyProfile(symbol) {
